1232635e198fc32ddb0bf2c657c26d3682b90f4c
esmeaa <[earthuraidoo@gmail.com protected]>
2025-06-04 20:36:05 +0100

    launch.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Launch from './pages/launch';
+import {Routes, Route} from "react-router-dom"; 
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Routes>
+          <Route path="/launch" element={<Launch />}/>
+        </Routes>
       
       </header>
     </div>
