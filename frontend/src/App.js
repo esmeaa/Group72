49d16c4222a8c78519b37c81b0979d8b2fa702e4
CheffieGithub <113442598+CheffieGithub@users.noreply.github.com>
2025-06-05 18:45:33 +0100

    ill just upload this but the css is merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,25 +9,24 @@ import Admin from './pages/adminDashboard';
 import HomeSeekerRegister from './pages/HomeSeekerRegister';
 import BuilderRegister from './pages/BuilderRegister';
 import AdminRegister from './pages/AdminRegister';
+import { BuilderDash } from './pages/builderDashboard';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <TempNav />
-
-        
-      <Routes>
-      <Route path='/launch' element={<Launch/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/admin' element={<Admin/>}/>
-      <Route path='/HomeSeekerRegister' element={<HomeSeekerRegister/>}/>
-      <Route path='/BuilderRegister' element={<BuilderRegister/>}/>
-      <Route path='/AdminRegister' element={<AdminRegister/>}/> 
-      </Routes> 
-
+        <TempNav />
       </header>
+      <Routes>
+        <Route path='/launch' element={<Launch/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/admin' element={<Admin/>}/>
+        <Route path='/HomeSeekerRegister' element={<HomeSeekerRegister/>}/>
+        <Route path='/BuilderRegister' element={<BuilderRegister/>}/>
+        <Route path='/AdminRegister' element={<AdminRegister/>}/> 
+        <Route path='/builderDashboard' element={<BuilderDash/>}/>
+      </Routes> 
     </div>
   );
 }
--- frontend/src/tempNav.js
+++ frontend/src/tempNav.js
@@ -10,6 +10,7 @@ const tempNav = () => {
             <li><NavLink to="/launch" className="nav-link"> Launch</NavLink></li>
             <li><NavLink to="/login" className="nav-link"> LoginPage</NavLink></li>
             <li><NavLink to="/admin" className="nav-link"> Admin Dashboard</NavLink></li>
+            <li><NavLink to="/builderDashboard" className="nav-link"> Builder Dashboard</NavLink></li>
             <li><NavLink to="/HomeSeekerRegister" className="nav-link"> Home Seeker Register</NavLink></li>
             <li><NavLink to="/BuilderRegister" className="nav-link"> Builder Register</NavLink></li>
             <li><NavLink to="/AdminRegister" className="nav-link"> Admin Register</NavLink></li>
