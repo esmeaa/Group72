f6eb2f1e861cc1a9702e01365ffc6bb8b7db41cf
esmeaa <[earthuraidoo@gmail.com protected]>
2025-06-05 12:38:45 +0100

    Added ubuntulogo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import LoginPage from './pages/LoginPage';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LoginPage />
+        
+      {/* <Routes>
+      <Route path="/Login" element={<LoginPage />} />
+
+      </Routes> */}
       </header>
     </div>
   );
