56fb30a9523579235358c86c99f661974c838585
CheffieGithub <113442598+CheffieGithub@users.noreply.github.com>
2025-06-06 18:00:08 +0100

    builder touch ups

--- frontend/src/pages/builderDashboard.jsx
+++ frontend/src/pages/builderDashboard.jsx
@@ -1,26 +1,52 @@
 import { React, useState } from 'react'
 import styles from "./builderDashboard.module.css";
+import { Hammer, Check, Wallet } from "lucide-react";
 // Create an Action bar to use for all pages
 
 function BuilderDash() {
-  const [count, setCount] = useState(0)
-  const [active, setActive] = useState(0)
-  const [completed, setCompleted] = useState(0)
-  const [earnings, setEarnings] = useState(0)
+  const [count, setCount] = useState(0);
+  const [active, setActive] = useState(0);
+  const [completed, setCompleted] = useState(0);
+  const [earnings, setEarnings] = useState(0);
 
-  const [showPay, setShowPay] = useState(false)
+  const [showPay, setShowPay] = useState(false);
+  const [showAll, setShowAll] = useState(false);
 
-  const type = "builder"
-  const email = "johndoe@doe.com"
-  const phone = "+27 0000 000000"
+  const type = "builder";
+  const email = "johndoe@doe.com";
+  const phone = "+27-0000-000000";
 
-  const getApplications = all => {
-    var apps = []
-    if (all) {
-      return apps
-    }
-    return apps.slice(0, 3)
+  const application1 = {
+    title: "Screaming",
+    status: "denied",
   }
+
+  const application2 = {
+    title: "Shouting",
+    status: "approved",
+  }
+
+  const application3 = {
+    title: "Screeching",
+    status: "pending",
+  }
+
+  const application4 = {
+    title: "Shouting",
+    status: "approved",
+  }
+
+  const addSkill = (skill) => {
+    return;
+  };
+
+  const getApplications = () => {
+    var apps = [application1, application2, application3, application4];
+    if (showAll) {
+      return apps;
+    }
+    return apps.slice(0, 3);
+  };
   
   return (
     <div className={styles.builder_dash}>
@@ -28,7 +54,7 @@ function BuilderDash() {
         <article id={styles.profile} className={styles.holder}>
           <button>Edit Details</button>
           <div className={styles.icon}>
-          
+            
           </div>
           <span className={styles.welcome}>
             <span>Welcome back, John Doe!</span>
@@ -42,21 +68,21 @@ function BuilderDash() {
           <div className={`${styles.holder} ${styles.active}`}>
             <p>Active Projects</p>
             <div className={styles.icon}>
-
+              <Hammer size={40}/>
             </div>
             <p className={styles.value}>{active}</p>
           </div>
           <div className={`${styles.holder} ${styles.complete}`}>
             <p>Jobs Completed</p>
             <div className={styles.icon}>
-          
+              <Check size={40}/>
             </div>
             <p className={styles.value}>{completed}</p>
           </div>
           <div className={`${styles.holder} ${styles.pay}`}>
             <p>Total Earnings</p>
             <div className={styles.icon}>
-            
+              <Wallet size={40}/>
             </div>
             <p className={styles.value}>R {earnings}</p>
             <button onClick=""> View Playslip</button>
@@ -75,25 +101,22 @@ function BuilderDash() {
             <div className={styles.flex_item}>Engineering</div>
             <div className={styles.flex_item}>Engineering</div>
             <div className={styles.flex_item}>Engineering</div>
-            <button className={styles.flex_item}>+Add Skill</button>
+            <button onClick="" className={styles.flex_item}>+Add Skill</button>
           </div>
         </div>
 
         <div className={`${styles.application_holder} ${styles.holder}`}>
           <p>My Job Applications</p>
-          <button onClick="">View All</button>
-          <div className={`${styles.application} ${styles.pending}`}>
-            <p>Test</p>
-            <p className={styles.status}>Pending</p>
-          </div>
-          <div className={`${styles.application} ${styles.approved}`}>
-            <p>Test</p>
-            <p className={styles.status}>Approved</p>
-          </div>
-          <div className={`${styles.application} ${styles.denied}`}>
-            <p>Test</p>
-            <p className={styles.status}>Denied</p>
-          </div>
+          <button onClick={() => {setShowAll(!showAll)}}>View All</button>
+          {getApplications().map((application) => {
+            const appStatus = application.status;
+            return (
+            <div className={`${styles.application} ${styles[appStatus]}`}>
+              <p>{application.title}</p>
+              <p className={styles.status}>{appStatus}</p>
+            </div>
+            )
+            })}
         </div>
       </div>
     </div>
