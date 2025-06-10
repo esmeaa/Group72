5c74ee685359f7d371f5e7586d8062ca50b1e4da
jamie2tu <152534939+jamie2tu@users.noreply.github.com>
2025-06-10 12:19:42 +0100

    logo update

--- frontend/src/pages/launch.jsx
+++ frontend/src/pages/launch.jsx
@@ -19,6 +19,31 @@
 // // export default launch;
 
 
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import hands from '../images/logo/hands.png';
+// import house from '../images/logo/house.png';
+// import title from '../images/logo/ubuntuTitle.png';
+// import styles from './launch.module.css';
+
+// const Launch = () => {
+//   const navigate = useNavigate();
+
+//   const handleClick = () => {
+//     navigate('/login');
+//   };
+
+//   return (
+//     <div className={styles.launch_container} onClick={handleClick}>
+//       <img src={title} alt="Ubuntu Homes Title" className={styles.title} />
+//       <img src={house} alt="House Icon" className={styles.house} />
+//       <img src={hands} alt="Helping Hands Icon" className={styles.hands} />
+//     </div>
+//   );
+// };
+
+// export default Launch;
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import hands from '../images/logo/hands.png';
