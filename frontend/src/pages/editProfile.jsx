87f2f554049819777018baf01e133b62e9defa27
CheffieGithub <113442598+CheffieGithub@users.noreply.github.com>
2025-06-10 19:20:09 +0100

    the options themselves

--- frontend/src/pages/editProfile.jsx
+++ frontend/src/pages/editProfile.jsx
@@ -14,7 +14,11 @@ const EditProfile = ({ userId, onProfileUpdate }) => {
     skills: [],
     newSkill: '',
     disability: false,
-    disability_details: ''
+    disability_details: '',
+    accessibility: false,
+    fontsize: '',
+    language: '',
+    darkmode: false,
   });
 
   const [loading, setLoading] = useState(true);
@@ -186,6 +190,48 @@ const EditProfile = ({ userId, onProfileUpdate }) => {
           />
           Own Pets?
         </label>
+
+        <label className={styles.checkbox_label}>
+          <input
+            name="accessibility"
+            type="checkbox"
+            checked={formData.accessibility}
+            onChange={handleChange}
+          />
+          Enable Accessibility Options
+        </label>
+
+        {formData.accessibility && (
+          <div>
+            <div className={styles.full_field}>
+              <label className={styles.label}>Font Size:</label>
+              <select name="fontsize" value={formData.fontsize} onChange={handleChange}>
+                <option value="">Select</option>
+                <option value="10">Small</option>
+                <option value="11">Normal</option>
+                <option value="12">large</option>
+                <option value="14">Extra large</option>
+              </select>
+            </div>
+            <div className={styles.full_field}>
+              <label className={styles.label}>Preferred Language:</label>
+              <select name="language" value={formData.language} onChange={handleChange}>
+                <option value="">Select</option>
+                <option value="eng">English</option>
+                <option value="zlu">isiZulu</option>
+              </select>
+            </div>
+            <label className={styles.checkbox_label}>
+              <input
+                name="darkmode"
+                type="checkbox"
+                checked={formData.darkmode}
+                onChange={handleChange}
+              />
+              Enable Darkmode
+            </label>
+          </div>
+        )}
 
         <button type="submit" className={styles.submit_btn}>Save Changes</button>
       </form>
